858d3410f170427b19527232985303f2446ab948
Nurdin <mrsunnysummer@gmail.com>
2024-10-09 23:49:21 +0700

    feat: create search bar search view in menu activity

--- app/src/main/java/com/nudriin/myappbar/MenuActivity.kt
+++ app/src/main/java/com/nudriin/myappbar/MenuActivity.kt
@@ -1,12 +1,34 @@
 package com.nudriin.myappbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.nudriin.myappbar.databinding.ActivityMenuBinding
 
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // with binding disini maksudnya biar bisa
+        // binding.searchView atau binding.searchBar
+        with(binding) {
+            // seperti disini langsung searchView tanpa mengetikan binding
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                //aksi ketika input di-submit
+                .setOnEditorActionListener { textView, actionId, event ->
+                    // mengambil input dari SearchView menggunakan searchView.text
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
     }
 }
