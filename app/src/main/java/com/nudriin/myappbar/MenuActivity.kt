ec42c72bcc8819f004ba253dc26dfe0391dd8238
Nurdin <mrsunnysummer@gmail.com>
2024-10-09 23:29:16 +0700

    feat: create custom app bar

---
+++ app/src/main/java/com/nudriin/myappbar/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.nudriin.myappbar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
